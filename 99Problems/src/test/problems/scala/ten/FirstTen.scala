b5eff1e833f0cee83564a876b3e5376cce5e44f2
swapanp <swapanp@rssoftware.co.in>
2016-08-16 15:19:57 +0530

    added probs 2-4

--- 99Problems/src/test/problems/scala/ten/FirstTen.scala
+++ 99Problems/src/test/problems/scala/ten/FirstTen.scala
@@ -2,11 +2,60 @@ package test.problems.scala.ten
 
 object FirstTen {
   
-  //Find the last element of a list.
+   /**
+   * P01 (*) Find the last element of a list.
+	 * Example:
+	 * scala> last(List(1, 1, 2, 3, 5, 8))
+	 * res0: Int = 8
+   */
   def last[A](list: List[A]): Option[A] = list match {
     case Nil => None
     case x :: Nil => Some(x)
     case _ :: xs => last(xs)
+  }
+  
+  /**
+   * P02 (*) Find the last but one element of a list.
+   *   Example:
+   *   scala> penultimate(List(1, 1, 2, 3, 5, 8))
+   *   res0: Int = 5
+   */
+  def getBeforeLast[A](l : List[A]) : Option[A] = l match {
+    case Nil => None
+    case x::Nil => None
+    case x::y::Nil => Some(x)
+    case _::t => getBeforeLast(t)
+  }
+  
+  /**
+   * P03 (*) Find the Kth element of a list.
+   **   By convention, the first element in the list is element 0.
+   *   Example:
+   *   
+   *   scala> nth(2, List(1, 1, 2, 3, 5, 8))
+   *   res0: Int = 2
+   **/
+  def kth[A](list : List[A], k : Int) : Option[A] = list match {
+    case Nil => None
+    case h::t => 
+      if (k < 0 ) throw new Exception("Invalid index") 
+      else if (k == 0) Some(h) 
+      else kth(t, k - 1)
+  }
+  
+  /**
+   * P04 (*) Find the number of elements of a list.
+   *   Example:
+   *   scala> length(List(1, 1, 2, 3, 5, 8))
+   *   res0: Int = 6
+   **/
+  def length[A](list: List[A]): Int = {
+    def lengthAux(list : List[A], count : Int) : Int = list match {
+      case Nil => count
+      case _:: t => lengthAux(t, count + 1)
+    }
+    
+    lengthAux(list, 0)
   }
   
 }
--- 99Problems/src/test/problems/scala/ten/Test.scala
+++ 99Problems/src/test/problems/scala/ten/Test.scala
@@ -6,7 +6,11 @@ object Test {
   
   def main(args: Array[String]): Unit = {
     println("Test List: %s".format(list))
-    println("Problem 01: Last Element of the list: %s".format(FirstTen.last(list)))
+    println("Problem 01: Last Element of the list: %s".format(FirstTen.last(list).getOrElse("None")))
+    println("Problem 02: Element before Last Element of the list: %s".format(FirstTen.getBeforeLast(list).getOrElse("None")))
+    println("Problem 03: K(%d)th Element of the list: %s".format(4, FirstTen.kth(list, 4).getOrElse("None")))
+    println("Problem 04: Length of the list: %s".format(FirstTen.length(list)))
   }
+  
   
 }
